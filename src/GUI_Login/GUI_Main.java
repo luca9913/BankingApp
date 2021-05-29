ab65b5ecd31b8a15159ab8d69180a8560c05b5b8
lucalm <luca_trumpf@protonmail.com>
2021-05-29 11:18:02 +0200

    Merged two projects back into one, deleted BankingApp(new)

---
+++ src/GUI_Login/GUI_Main.java
@@ -0,0 +1,12 @@
+package GUI_Login;
+
+public class GUI_Main {
+
+    public static void main(String[] args) {
+        GUI_Login newView = new GUI_Login();
+        newView.setVisible(true);
+    }
+
+
+
+}
