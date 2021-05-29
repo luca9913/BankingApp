7cc983ac1f1a0aa969ec8ae0eb8b6a1ec65db3d6
LucForster <luc.forster2@web.de>
2021-05-29 12:14:05 +0200

    Create Login.java

---
+++ src/Login/Login.java
@@ -0,0 +1,17 @@
+package Login;
+
+public class Login {
+
+    private String username;
+    private String password;
+    private String pwHash;
+    private int UserID;
+
+
+
+
+    private String hashen(String password){
+
+        return pwHash;
+    }
+}
