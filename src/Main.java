9e4b7c93606b0d9c5a81cc491c925fa8bb760aa6
AaronLelouch <aaron.lacks@outlook.de>
2021-06-03 13:27:44 +0200

    Main angepasst und in scr verschoben

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-package GUI.GUI_Login;
+import GUI.GUI_Login.GUI_Login;
+import GUI.GUI_Banker.GUI_Banker;
+import GUI.GUI_Customer.GUI_Customer;
 
 import java.awt.*;
 
-public class GUI_Main {
+public class Main {
 
     public static void main(String[] args) {
         GUI_Login newView = new GUI_Login();
