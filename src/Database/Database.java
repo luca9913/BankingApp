02f1a076ebd719744c77e4d098251c19935152a3
lucalm <luca_trumpf@protonmail.com>
2021-06-03 02:30:17 +0200

    Implemented both Database classes and first methods to read and write data. Also added a little DatabaseTest example to run.

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -3,74 +3,199 @@ package Database;
 import java.sql.*;
 import java.nio.file.*;
 
-abstract class Database{
-    final static String DRIVERSTRING = "jdbc:sqlite:";
-    static Path path;
+abstract class Database {
+    final String DRIVER = "jdbc:sqlite:";
+    final String FOLDER = "src/data/";
     static Connection conn;
+    static Statement state;
 
-    static Connection initialize(String p){ //Die Pfad-Strings könnten irgendwo als final deklariert werden
-        path = Paths.get(p);
-        if(!(path.toString().isEmpty())){
-            try {
-                conn = DriverManager.getConnection(DRIVERSTRING + path);
-            }catch(SQLException e){
-                System.err.println("Fehler beim Initialisieren der Datenbank.");
-                System.err.print("Fehlermeldung: ");
-                e.printStackTrace();
-            }
-        }else{
-            System.err.println("Instance of Authentication Database already created. Only one Instance allowed.");
+    boolean executeCustomQuery(String sql){
+        try {
+            return state.execute(sql); //Gibt true zurück, wenn die Abfrage Ergebnisse liefert. Gibt false zurück, wenn es sich um ein UPDATE handelte oder keine Werte zurückkamen.
+        }catch(SQLException e){
+            System.err.println("Beim Ausführen der Abfrage ist ein Fehler aufgetreten.");
+            System.err.print("Fehlermeldung: ");
+            e.printStackTrace();
+            return false;
         }
-        return conn;
     }
-    abstract ResultSet getData(String sql);
-    abstract int updateData(String sql);
+
+    String[] rsToStringArray(ResultSet rs){
+        try {
+            ResultSetMetaData rsmeta = rs.getMetaData();
+            int columns = rsmeta.getColumnCount();
+            String[] result = new String[columns];
+            for (int i = 1; i <= columns; i++) { //Möglichkeit finden Zeilen & Spalten auszulesen
+                result[i-1]= rs.getString(i);
+            }
+            return result;
+        }catch(SQLException e){
+            System.err.println("Fehler beim Übertragen eines ResultSets in einen StringArray.");
+            System.err.print("Fehlermeldung: ");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    String[] getTables(){
+        try{
+            ResultSet rs = state.executeQuery("SELECT name FROM sqlite_master WHERE type = 'table' AND name NOT LIKE 'sqlite_%'");
+            return rsToStringArray(rs);
+        }catch(SQLException e){
+            System.err.println("Fehler beim Auslesen der Tabellen:");
+            System.err.print("Fehlermeldung: ");
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
 
 class AuthBase extends Database{
 
-    private AuthBase(){} //Konstruktor auf private setzen, damit nur eine Instanz erzeugt werden kann
+    static Path path = Paths.get("");
+    PreparedStatement insertbanker, insertcustomer;
 
-    ResultSet getData(String sql){
-        ResultSet rs = null;
-
-        try {
-            Statement state = conn.createStatement();
-            rs = state.executeQuery(sql);
+    private AuthBase(){
+        this.path = Paths.get(FOLDER + "auth.db");
+        try{
+            this.conn = DriverManager.getConnection(DRIVER + path);
+            this.state = conn.createStatement();
+            insertbanker = conn.prepareStatement("INSERT INTO user(pw_hash, banker_id) VALUES (?,?)");
+            insertcustomer = conn.prepareStatement("INSERT INTO user(pw_hash, customer_id) VALUES (?,?)");
         }catch(SQLException e){
-            System.err.println("Fehler beim Ausführen des SQL-Statements.");
-            System.err.print("Fehlermeldung: ");
+            System.err.println("Beim Initialisieren der Authentifizierungsdatenbank ist folgender Fehler aufgetreten: ");
             e.printStackTrace();
+            System.err.println("Das Programm wird beendet...");
+            System.exit(1);
         }
-        return rs;
+    } //Konstruktor auf private setzen, damit nur eine Instanz erzeugt werden kann
+
+    static AuthBase initialize(){
+        if(path.toString().isEmpty()){
+            AuthBase auth = new AuthBase();
+            return auth;
+        }else{
+            System.err.println("Authentifizierungs-Datenbank wurde bereits erstellt. Es ist nur eine Instanz erlaubt!");
+            return null;
+        }
     }
 
-    int updateData(String sql){
-        int changedlines = 0;
-
+    String[] getAuthSet(int uid){
         try {
-            Statement state = conn.createStatement();
-            changedlines = state.executeUpdate(sql);
+            ResultSet rs = state.executeQuery("SELECT * FROM user WHERE user_id = " + uid);
+            return rsToStringArray(rs);
         }catch(SQLException e){
             System.err.println("Fehler beim Ausführen des SQL-Statements.");
             System.err.print("Fehlermeldung: ");
             e.printStackTrace();
+            return null;
         }
-        return changedlines;
+    }
+
+    boolean insertUser(String pw, int id, String function){
+        try {
+            int hash = pw.hashCode();
+
+            switch(function){
+                case "banker":
+                    insertbanker.setInt(1, hash);
+                    insertbanker.setInt(2, id);
+                    insertbanker.execute();
+                    break;
+                case "customer":
+                    insertcustomer.setInt(1, hash);
+                    insertcustomer.setInt(2, id);
+                    insertcustomer.execute();
+                    break;
+            }
+            return true;
+        }catch(SQLException e){
+            System.err.println("Fehler beim Einfügen des neuen Benutzers in die Datenbank.");
+            System.err.print("Fehlermeldung: ");
+            e.printStackTrace();
+            return false;
+        }
     }
 }
+
+class ProdBase extends Database{
+
+    static Path path = Paths.get("");
+    PreparedStatement insertaccount, insertbanker, insertcustomer, insertrelease, inserttransfer;
+
+    private ProdBase(){
+        this.path = Paths.get(FOLDER + "production.db");
+        try{
+            this.conn = DriverManager.getConnection(DRIVER + path);
+            this.state = conn.createStatement();
+            insertaccount = conn.prepareStatement("INSERT INTO account(type, balance, dispo, transfer_limit, owner, banker_id) VALUES (?,0,?,?,?,?)");
+        }catch(SQLException e){
+            System.err.println("Beim Initialisieren der Stammdatenbank ist folgender Fehler aufgetreten: ");
+            e.printStackTrace();
+            System.err.println("Das Programm wird beendet...");
+            System.exit(1);
+        }
+    } //Konstruktor auf private setzen, damit nur eine Instanz erzeugt werden kann
+
+    static ProdBase initialize(){
+        if(path.toString().isEmpty()){
+            ProdBase prod = new ProdBase();
+            return prod;
+        }else{
+            System.err.println("Stammdatenbank wurde bereits erstellt. Es ist nur eine Instanz erlaubt!");
+            return null;
+        }
+    }
+
+    String[] getAccountData(int accid){
+        try {
+            ResultSet rs = state.executeQuery("SELECT * FROM account WHERE account_id = " + accid);
+            return rsToStringArray(rs);
+        }catch(SQLException e){
+            System.err.println("Fehler beim Ausführen des SQL-Statements.");
+            System.err.print("Fehlermeldung: ");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    //Weitere Abfragen hinzufügen (für jede Tabelle)
+
+    boolean insertAccount(String type, int dispo, double transferlimit, int oid, int bid){
+        try {
+            insertaccount.setString(1, type);
+            insertaccount.setInt(2, dispo);
+            insertaccount.setDouble(3, transferlimit);
+            insertaccount.setInt(4, oid);
+            insertaccount.setInt(5, bid);
+            return insertaccount.execute();
+        }catch(SQLException e){
+            System.err.println("Fehler beim Einfügen des neuen Benutzers in die Datenbank.");
+            System.err.print("Fehlermeldung: ");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    //Weitere Eingaben hinzufügen
+
+    //Aktualisierungen hinzufügen
+}
+
 
 class DatabaseTest{
 
     public static void main(String[] args){
-        try {
-            Connection test = AuthBase.initialize("src/data/auth.db");
-            ResultSet rs = test.createStatement().executeQuery("SELECT * from user");
-            System.out.println("ID: " + rs.getString("user_id"));
-            System.out.println("PW Hash: " + rs.getString("pw_hash"));
-        }catch(SQLException e){
-            System.err.println("Fehler beim Auslesen der Datenbank!");
-            e.printStackTrace();
+        AuthBase auth = AuthBase.initialize(); //Datenbank initialisieren
+        System.out.println(auth.path.toString()); //Pfad ausgeben
+        AuthBase test = AuthBase.initialize(); //Private Konstruktor demonstrieren
+        auth.executeCustomQuery("UPDATE user SET customer_id = 485 WHERE user_id = 1");
+
+        ProdBase prod = ProdBase.initialize(); //Stammdatenbank initialisieren
+        String[] prodtables = prod.getTables(); //Tabellen abfragen
+        for(int i = 0; i < prodtables.length; i++){ //Tabellennamen ausgeben
+            System.out.println("Tabelle " + (i+1) + ": " + prodtables[i]);
         }
+        prod.insertAccount("Girokonto", 200, 9999.99, 1, 3);
     }
 }
