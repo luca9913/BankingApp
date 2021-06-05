e1d050ed0f5e150f3243bddad5f79184acf9c1f1
AaronLelouch <aaron.lacks@outlook.de>
2021-06-05 14:25:51 +0200

    GUI_Customer angepasst

--- src/GUI/GUI_Customer/GUI_Customer.java
+++ src/GUI/GUI_Customer/GUI_Customer.java
@@ -3,4 +3,16 @@ package GUI.GUI_Customer;
 import javax.swing.*;
 
 public class GUI_Customer extends JFrame {
+    private JTabbedPane tabbedPaneMain;
+    private JPanel Hauptpanel;
+    private JPanel JPanelLinks;
+    private JPanel JPanelRechts;
+    private JList list1;
+    private JTextField txtSearch;
+    private JTable table1;
+    private JButton aktualisierenButton;
+    private JTextField txtAccNr;
+    private JTextField txtTurnoverNr;
+    private JTextField txtAmount;
+    private JTextField txtUsage;
 }
