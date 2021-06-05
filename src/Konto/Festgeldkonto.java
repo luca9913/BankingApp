e1a06e3a1e1ba69aacfa5416688bd9378ab3bcde
Julius <theknox420000@gmail.com>
2021-06-05 12:21:34 +0200

    Konto Klassen

---
+++ src/Konto/Festgeldkonto.java
@@ -0,0 +1,31 @@
+package Konto;
+
+public class Festgeldkonto {
+    double kontostand;
+    public Festgeldkonto(){
+        kontostand = 0;
+    };
+
+    //Funktion, um alle Konten in der GUI zu aktualisieren
+    private void syncAccounts(){
+
+    }
+
+    //Funktion, um einzuzahlen
+    private void deposit(double sum){
+
+    }
+
+    //Funktion, um die aktuellen Kontostände anzuzeigen
+    //Offene Frage: Alle Kontostände oder nur der Kontostand eines Kontos
+    private void showFinances(int accID){
+
+    }
+
+    //Zeigt Kontobewegungen (z.B. letzte Einzahlungen)
+    //Offene Frage: Alle Kontobewegungen oder nur die Kontobewegungen eines Kontos
+    private void showAccMovement(int accID){
+
+    }
+
+}
