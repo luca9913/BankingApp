ecefc0839624eff55b2499a0554e6dcf4bae0fb6
404_Brain <7th_i@online.de>
2021-06-05 11:24:33 +0200

    Action wenn LoginButton gedrückt hinzugefügt + readDatabase in LoginKlasse bearbeitet Konstruktur für Login mit Datenbank instanz working on main to ensure perfomantie

--- src/Login/Login.java
+++ src/Login/Login.java
@@ -8,10 +8,12 @@ public class Login {
     private int pwHash;
     private int userID = 0;
     private boolean valid;
-    AuthBase datenbankb;
+    AuthBase datenbank;
 
     public Login(AuthBase datenbank)//Methode readFromGUI: liest userID und password aus GUI Eingabe
-
+    {
+        this.datenbank = datenbank;
+    }
     //hashed das Passwort
 
     private int hashen(String password){
@@ -21,10 +23,12 @@ public class Login {
 
     //readDatabase vergleicht die Eingabe mit der Datenbank und gibt mit "1" zurück ob ein passender Tupel gefunden wurde.
     public boolean readDatabase(int userID, String password){
-        pwHash = hashen(password);
-        // (SELECT user_id FROM user WHERE pw_HASH == pwHash && user_ID == userID)
-
-        return valid;
+        pwHash = hashen(password); //passwort zu hash umwandeln
+        if (pwHash == datenbank.getHash(userID)) //hash mit Datenbank abgleichen
+        {
+            return true;
+        }
+        return false;
     }
 
     //sendToGUI
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,6 @@
 package Main;
 
+import Database.*;
 import GUI.GUI_Login.GUI_Login;
 import GUI.GUI_Banker.GUI_Banker;
 import GUI.GUI_Customer.GUI_Customer;
@@ -11,6 +12,7 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
+        AuthBase authBase = AuthBase.initil
         Login loginObjekt = new Login();
         GUI_Login newView = new GUI_Login(loginObjekt);
         newView.setVisible(true);
