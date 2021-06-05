3f0af50bb2818100d9b6ace39159aec02ce739d6
AaronLelouch <aaron.lacks@outlook.de>
2021-06-05 12:39:37 +0200

    GUI_Customer angepasst

--- src/GUI/GUI_Customer/GUI_Customer.java
+++ src/GUI/GUI_Customer/GUI_Customer.java
@@ -10,10 +10,10 @@ import java.util.Random;
 public class GUI_Customer extends JFrame{
     private JPanel panel1;
     private JTabbedPane tabbedPane1;
-    private JList list1;
-    private JTable table1;
-    private JButton aktualisierenButton;
-    private JTextField textField1;
+    private JButton btnRefresh;
+    private JTextField txtSearch;
+    private JList listAccounts;
+    private JTable tableTurnover;
 
     public GUI_Customer() {
 
