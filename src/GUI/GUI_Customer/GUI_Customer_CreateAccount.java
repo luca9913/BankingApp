634f77c3b821b3dce00675ee4abf51e29d5f1ea1
AaronLelouch <aaron.lacks@outlook.de>
2021-06-09 15:03:24 +0200

    GUI_Customer un GUI_Customer_DeleteAccount und GUI_Customer_CreateAccount angepasst.

--- src/GUI/GUI_Customer/GUI_Customer_CreateAccount.java
+++ src/GUI/GUI_Customer/GUI_Customer_CreateAccount.java
@@ -1,10 +1,31 @@
 package GUI.GUI_Customer;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI_Customer_CreateAccount {
     private JPanel panel1;
     private JRadioButton girokontoRadioButton;
     private JButton abbrechenButton;
     private JButton kontoAnlegenButton;
+
+    public GUI_Customer_CreateAccount() {
+
+        //Action für den Button "Konto erstellen"
+        kontoAnlegenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        //Action für den Button "Abbrechen"
+        abbrechenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
--- src/GUI/GUI_Customer/GUI_Customer_DeleteAccount.java
+++ src/GUI/GUI_Customer/GUI_Customer_DeleteAccount.java
@@ -1,10 +1,31 @@
 package GUI.GUI_Customer;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI_Customer_DeleteAccount {
     private JList listAccountToRemove;
     private JList listAccountToReceive;
     private JButton btnRemoveAccount;
     private JButton btnCancel;
+
+    public GUI_Customer_DeleteAccount() {
+
+        //Action für den Button "Konto löschen"
+        btnRemoveAccount.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        //Action für den Button "Abbrechen"
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
