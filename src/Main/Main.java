48c8fcc3cad99da15e9dd62bac1598c11448a28f
AaronLelouch <aaron.lacks@outlook.de>
2021-06-16 14:35:12 +0200

    Anpassen der Login GUI, sodass sie in der Mitte des Bildschirms geöffnet wird.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,30 +15,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        GUI_Customer_Connector.openCustomer();
-
         AuthBase authBase = AuthBase.initialize();
         ProdBase prodBase = ProdBase.initialize();
         Login log = new Login(authBase);
         GUI_Login wneView = new GUI_Login(log);
         wneView.setVisible(true);
 
-
-
-        Person user = null;
-        ProdBase prodBase = ProdBase.initialize();
-        Login loginObjekt = new Login(authBase, user);
-        GUI_Login newView = new GUI_Login(loginObjekt);
-        newView.setVisible(true);
-
-        if(user.getUid() < 1000)
-        {
-           GUI_Banker ewnView = new GUI_Banker();
-           ewnView.setVisible(true);
-        }else
-        {
-            GUI_Customer wneView = new GUI_Customer();
-            wneView.setVisible(true);
-        }
     }
 }
