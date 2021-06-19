d7ea1909cb059ba7df64b5bbda31f791a4c23fb7
AaronLelouch <aaron.lacks@outlook.de>
2021-06-19 13:05:39 +0200

    Einfügen der Javadoc Notation bei den Hilfsklassen der GUIs und der GUI_Customer, GUI_Customer_DeleteAccoutn und der GUI_Customer_CreateAccount

--- src/GUI/GUI_Customer/GUI_Customer.java
+++ src/GUI/GUI_Customer/GUI_Customer.java
@@ -51,13 +51,16 @@ public class GUI_Customer extends JFrame {
 
 
     /**
-     * Dieser Konstruktor ist für die Actions und weitere Optionen des Gui´s des Customers zuständig.
+     * Dieser Konstruktor ist für die Actions und weitere Optionen des CUstomer-Gui´s zuständig.
      */
     public GUI_Customer() {
 
         initialize();
 
-        //Action für den Button "Aktualisieren im Tab Finanzübersicht"
+        /**
+         * Die Action des Buttons "Aktualisieren" im Tab Finanzübersicht ist für das Aktualisieren der Tabelle und der Konten zuständig.
+         * Wenn sich Änderungen ergeben haben dann werden diese durch das Aktualisieren in der Tabelle übernommen.
+         */
         btnRefresh1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -65,7 +68,9 @@ public class GUI_Customer extends JFrame {
             }
         });
 
-        //Action für den Button "Überweisen im Tab überweisen"
+        /**
+         * Die Action des Buttons "Überweisen" im Tab Überweisen triggert die Überweisung.
+         */
         btnTransfer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -73,7 +78,10 @@ public class GUI_Customer extends JFrame {
             }
         });
 
-        //Action für den Button "Aktualisieren im Tab Konten"
+        /**
+         * Die Action des Buttons "Aktualisieren" im Tab Konten ist für das Aktualisieren der Tabelle und der Konten zuständig.
+         * Wenn sich Änderungen ergeben haben dann werden diese durch das Aktualisieren in der Tabelle übernommen.
+         */
         btnRefresh2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -81,29 +89,35 @@ public class GUI_Customer extends JFrame {
             }
         });
 
-        //Action für den Button "Konto auflösen im Tab Konten"
+        /**
+         * Die Action des Buttons "Konto auflösen" im Tab Konten öffnet das Neben-Gui´s GUI_Customer_DeleteAccount
+         */
         btnDeleteAccount.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
                 GUI_Customer_Connector.openDelete();
-                //setEnabled(false);
 
             }
         });
 
-        //Action für den Button "Neues Konto erstellen im Tab Konten"
+        /**
+         * Die Action des Buttons "Neues Konto erstellen" im Tab Konten öffnet das Neben-Gui´s GUI_Customer_CreateAccount
+         */
         btnNewAccount.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
                 GUI_Customer_Connector.openCreate();
-                //setEnabled(false);
 
             }
         });
 
-        //Action für den Button "Disporahmen im Tab Konten"
+        /**
+         * Die Action des Buttons "Dispo- / Kreditrahmen ändern" im Tab Konten ist für das Ändern und Speichern
+         * des Disporahmens eines vorher selektierten Kontos und die Aktivierung bzw. Deaktivierung des Textfeldes
+         * für den Disporahmen zuständig.
+         */
         btnDispoNew.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -111,7 +125,11 @@ public class GUI_Customer extends JFrame {
             }
         });
 
-        //Action für den Button "Überweisungsrahmen ändern im Tab Konten"
+        /**
+         * Die Action des Buttons "Überweisungsrahmen ändern" im Tab Konten ist für das Ändern und Speichern
+         * des Überweisungsrahmens eines vorher selektierten Kontos und die Aktivierung bzw. Deaktivierung
+         * des Textfeldes für den Überweisungsrahmen zuständig.
+         */
         btnChangeTransferLimit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -119,7 +137,10 @@ public class GUI_Customer extends JFrame {
             }
         });
 
-        //Action für den Button "Aktualisieren im Tab Benutzerdaten"
+        /**
+         * Die Action des Buttons "Aktualisieren" im Tab Benutzerdaten ist für das Aktualisieren der Tabelle und der Konten zuständig.
+         * Wenn sich Änderungen ergeben haben dann werden diese durch das Aktualisieren in der Tabelle übernommen.
+         */
         btnRefresh3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -127,7 +148,10 @@ public class GUI_Customer extends JFrame {
             }
         });
 
-        //Action für den Button "Persönliche Daten ändern im Tab Benutzerdaten"
+        /**
+         * Die Action des Buttons "Persönliche Daten ändern" im Tab Benutzerdaten ist für die Aktivierung der
+         * Textfelder für die Änderung der Benutzerdaten im Tab Benutzerdaten zuständig.
+         */
         btnCustomerDataChanges.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -135,7 +159,10 @@ public class GUI_Customer extends JFrame {
             }
         });
 
-        //Action für den Button "Speichern im Tab Benutzerdaten"
+        /**
+         * Die Action des Buttons "Speichern" im Tab Benutzerdaten ist für die Speicherung und der
+         * Deaktivierung der Textfelder für die Änderung der Benutzerdaten im Tab Benutzerdaten zuständig.
+         */
         btnSave.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
