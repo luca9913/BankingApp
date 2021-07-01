0a420098c46b2b4624388397e5f2ea1b4a4a689a
AaronLelouch <aaron.lacks@outlook.de>
2021-07-01 23:55:33 +0200

    x

--- src/GUI/GUI_Customer/GUI_Customer.java
+++ src/GUI/GUI_Customer/GUI_Customer.java
@@ -338,6 +338,7 @@ public class GUI_Customer extends JFrame {
                     txtAddress.setEditable(false);
                     txtPhone.setEditable(false);
                     txtMail.setEditable(false);
+                    btnCustomerDataChanges.setText("Persönliche Daten ändern");
                 }
                 else if(hm.onlyString(txtName.getText(), false, 2) == false ||
                         hm.onlyString(txtPrename.getText(), false, 2) == false ||
